3ed3cd939e44835cc363fe545cacc9dd1f4da428
anajuglari <ana.juglari@escola.pr.gov.br>
2026-04-28 09:25:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 import { registerRootComponent} fromm'expo';
 import App from './App';
+// registerComponent('main' , () => App);
+// It also ensures that whether you load the app in Expo Go or in a native biild,
+// the environment is set up appropriately
 registerRootCompnent(App);
